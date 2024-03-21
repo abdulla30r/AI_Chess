39c220c546c191c0872b70a62096e7e94063d2e9
Abdulla Rahman <abdulla30r@gmail.com>
2024-03-21 17:41:28 +0600

    pawn moves validated

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -28,34 +28,37 @@ class GameState():
     '''
     move a piece using move parameter. this will not work for pawn promotion, castling, en-passant
     '''
+
     def makeMove(self, move):
-        self.board[move.startRow][move.startCol] = "--"     # make blank in source
+        self.board[move.startRow][move.startCol] = "--"  # make blank in source
         self.board[move.endRow][move.endCol] = move.pieceMoved  # put piece in destination
-        self.moveLog.append(move)   # log the move, so we can see history or undo move
-        self.whiteToMove = not self.whiteToMove     # swap players
+        self.moveLog.append(move)  # log the move, so we can see history or undo move
+        self.whiteToMove = not self.whiteToMove  # swap players
 
     '''
       undo the last move
     '''
+
     def undoMove(self):
-        if len(self.moveLog) > 0:   # make sure there is a move to undo
+        if len(self.moveLog) > 0:  # make sure there is a move to undo
             move = self.moveLog.pop()
             self.board[move.startRow][move.startCol] = move.pieceMoved
             self.board[move.endRow][move.endCol] = move.pieceCaptured
-            self.whiteToMove = not self.whiteToMove     # swap players
+            self.whiteToMove = not self.whiteToMove  # swap players
 
     def getValidMoves(self):
-        return self.getAllPossibleMoves()   # for now, we will not worry about checks
+        return self.getAllPossibleMoves()  # for now, we will not worry about checks
 
     '''
     All moves without considering check
     '''
+
     def getAllPossibleMoves(self):
-        moves = [Move((6, 4), (4, 4), self.board)]
-        for r in range(len(self.board)):    # number of rows
-            for c in range(len(self.board[r])):     # number of cols
+        moves = []
+        for r in range(len(self.board)):  # number of rows
+            for c in range(len(self.board[r])):  # number of cols
                 turn = self.board[r][c][0]
-                if (turn == "w" and self.whiteToMove) and (turn == "b" and not self.whiteToMove):
+                if (turn == "w" and self.whiteToMove) or (turn == "b" and not self.whiteToMove):
                     piece = self.board[r][c][1]
                     if piece == "p":
                         self.getPawnMoves(r, c, moves)
@@ -66,8 +69,42 @@ class GameState():
     '''
     Get all the pawn moves at row,col and add these moves to the list
     '''
+
     def getPawnMoves(self, r, c, moves):
-        pass
+        if self.whiteToMove:  # white pawn moves
+
+            # 1 square pawn advance
+            if self.board[r - 1][c] == "--":
+                moves.append(Move((r, c), (r - 1, c), self.board))
+
+                # 2 square pawn advance
+                if (self.board[r - 2][c] == "--") and (r == 6):
+                    moves.append(Move((r, c), (r - 2, c), self.board))
+
+            # capture left corner
+            if (c > 0) and (self.board[r - 1][c - 1][0] == "b"):
+                moves.append(Move((r, c), (r - 1, c - 1), self.board))
+
+            # capture right corner
+            if (c < 7) and (self.board[r - 1][c + 1][0] == "b"):
+                moves.append(Move((r, c), (r - 1, c + 1), self.board))
+
+        else:  # black pawn moves
+            # 1 square pawn advance
+            if self.board[r + 1][c] == "--":
+                moves.append(Move((r, c), (r + 1, c), self.board))
+
+                # 2 square pawn advance
+                if (self.board[r + 2][c] == "--") and (r == 1):
+                    moves.append(Move((r, c), (r + 2, c), self.board))
+
+            # capture left corner
+            if (c > 0) and (self.board[r + 1][c - 1][0] == "w"):
+                moves.append(Move((r, c), (r + 1, c - 1), self.board))
+
+            # capture right corner
+            if (c < 7) and (self.board[r + 1][c + 1][0] == "w"):
+                moves.append(Move((r, c), (r + 1, c + 1), self.board))
 
     def getRookeMoves(self, r, c, moves):
         pass
@@ -79,10 +116,10 @@ class Move():
     # normally chess e vertical e 0-7 numbering kora. starting from white.
     # horizontal e a-h. called files. our board orders are not same. hence, mapping.
     ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}  # mapping dictionary
-    rowsToRanks = {v: k for k, v in ranksToRows.items()}    # reverse a dictionary
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}  # reverse a dictionary
 
     filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}  # mapping dictionary
-    colsToFiles = {v: k for k, v in filesToCols.items()}    # reverse a dictionary
+    colsToFiles = {v: k for k, v in filesToCols.items()}  # reverse a dictionary
 
     def __init__(self, startSq, endSq, board):
         self.startRow = startSq[0]
@@ -91,12 +128,13 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
-        self.moveID = self.startRow*1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
         print(self.moveID)
 
     '''
     Overriding the equals method
     '''
+
     def __eq__(self, other):
         if isinstance(other, Move):
             return self.moveID == other.moveID
