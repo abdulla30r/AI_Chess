e487402c4bb0fda82c0a89e740487e775014f170
Abdulla Rahman <abdulla30r@gmail.com>
2024-03-23 03:06:16 +0600

    knight move added

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -148,7 +148,16 @@ class GameState():
         self.getCommonMoves(directions, r, c, moves)
 
     def getKnightMoves(self, r, c, moves):
-        pass
+        knightMoves = ((2, -1), (2, 1), (-2, 1), (-2, -1), (1, -2), (1, 2), (-1, -2), (-1, 2))
+        allyColor = "w" if self.whiteToMove else "b"
+
+        for m in knightMoves:
+            endRow = r + m[0]
+            endCol = c + m[1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
 
     def getKingMoves(self, r, c, moves):
         pass
